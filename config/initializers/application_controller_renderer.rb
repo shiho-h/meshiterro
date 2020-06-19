db6f4445265446d6069c3408f82f28016e93480a
shiho-h <sisopiku@gmail.com>
2020-06-19 19:53:29 +0900

    finish5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7a6e1d0b56d47d38f6319a33a8233f3b84463a802a22670b7ce0e04ce2a31b59f402701c20e12052cfc61cf6b0a319cf154ea08960da05b90b258ed041082de4'
