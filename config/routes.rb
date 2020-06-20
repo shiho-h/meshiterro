7e8d7e982622ec5f7d414c3627d37388433de033
shiho-h <sisopiku@gmail.com>
2020-06-20 17:27:50 +0900

    finish5-6

--- config/routes.rb
+++ config/routes.rb
@@ -5,8 +5,7 @@ root 'post_images#index'
 
 resources :post_images, only: [:new, :create, :index, :show, :destroy] do
     resource :favorites, only: [:create, :destroy]
-    resources :post_comments, only: [:create, :destroy]
+    resource :post_comments, only: [:create, :destroy]
 end
-
-	resources :users, only: [:show]
+	resources :users, only: [:show, :edit, :update]
 end
